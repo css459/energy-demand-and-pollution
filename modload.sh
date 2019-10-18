4b21765f75ea79555a778dd64f1ea4c4375cba8f
Cole Smith <css459@nyu.edu>
2019-10-18 15:50:33 -0400

    Add Dumbo scripts

---
+++ modload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+module load spark/2.4.0
+module load git/1.8.3.1
+module load sbt/1.2.8
+module load scala/2.12.8
---
+++ submit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+spark-submit --class AirDataETL target/scala-2.12/bdad-final-project_2.12-0.1.jar
