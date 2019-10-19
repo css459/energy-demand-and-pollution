bb88fd2ba48a072e03ff0b005ed2eaa9485bfd7d
Cole Smith <css459@nyu.edu>
2019-10-18 21:37:18 -0400

    Changed submit script

--- submit.sh
+++ submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-spark2-submit --class bdad.etl.AirDataETL --master yarn --deploy-mode cluster --num-executors 20 --driver-memory 8G --executor-memory 2G target/scala-2.11/bdad-final-project_2.11-0.1.jar
+spark2-submit --name "AirDataETL" --class bdad.etl.AirDataETL --master yarn --num-executors 30 --driver-memory 5G --executor-memory 2G target/scala-2.11/bdad-final-project_2.11-0.1.jar
