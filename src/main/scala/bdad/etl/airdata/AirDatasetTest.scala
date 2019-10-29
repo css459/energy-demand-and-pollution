727f9f1b8f819d016f0d2735f3c053376adb71b5
Cole Smith <css@nyu.edu>
2019-10-29 14:09:16 -0400

    Added ETL NYU Classes Turn in direcory and content

--- src/main/scala/bdad/etl/airdata/AirDatasetTest.scala
+++ src/main/scala/bdad/etl/airdata/AirDatasetTest.scala
@@ -10,13 +10,24 @@ object AirDatasetTest extends App {
     .config("spark.master", "local")
     .getOrCreate
 
-  //  val criteria = Array("2019_EXPANDED")
-  //  val airdata = new AirDataset(AirDataset.ALL_YEARS, criteria)
+    val criteria = Array("2019_EXPANDED")
+    val airdata = new AirDataset(AirDataset.ALL_YEARS, criteria)
 
-  val airdata = new AirDataset(2019, "2019_EXPANDED/hourly_42101_2019.csv")
+  // val airdata = new AirDataset(2019, "2019_EXPANDED/hourly_42101_2019.csv")
 
   // Show Test DF
   airdata.df.cache.show(20, truncate = false)
+
+  // Show Describe Table
+  // airdata.df.describe().show(false)
+
+  // Show distinct criteria
+  // println("CRITERIA")
+  // airdata.df.select("criteria").distinct.show(100,false)
+
+  // Show distinct units
+  // println("UNITS")
+  // airdata.df.select("unit").distinct.show(false)
 
   // TODO: Why does this cause an exception?
   // airdata.pivotedDF(dropNull = true).count()
