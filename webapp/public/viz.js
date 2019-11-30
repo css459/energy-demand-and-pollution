4c4ab62eb91b94b0de40a76528f4a68cf6cf10bc
Andrii <adobro.developer@gmail.com>
2019-11-29 20:48:51 -0500

    config heatmap viz to our data

--- webapp/public/viz.js
+++ webapp/public/viz.js
@@ -1,11 +1,11 @@
 const {DeckGL, HexagonLayer} = deck;
 
 const deckgl = new DeckGL({
-    mapboxApiAccessToken: 'EXAMPLE_TOKEN_HERE',
+    mapboxApiAccessToken: 'pk.eyJ1IjoiYWRvYnJvc2h5bnNreWkiLCJhIjoiY2sza3gwcGY1MHlwMzNtcGhud3YydTBiZyJ9.PBkZqsJXUX2zm2oaCyiHYQ',
     mapStyle: 'mapbox://styles/mapbox/dark-v9',
-    longitude: -1.4157,
-    latitude: 52.2324,
-    zoom: 6,
+    longitude: -100.358887,
+    latitude: 42.113014,
+    zoom: 3,
     minZoom: 5,
     maxZoom: 15,
     pitch: 40.5
@@ -34,7 +34,7 @@ function renderLayer () {
         options[key] = Number(value);
     });
 
-    const hexagonLayer = new HexagonLayer({
+    const hexagonLayer = new HeatmapLayer({
         id: 'heat-map',
         colorRange: COLOR_RANGE,
         data,
@@ -50,7 +50,7 @@ function renderLayer () {
     });
 }
 
-d3.csv('https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv').then(res => {
-    data = res.map(d => [Number(d.lng), Number(d.lat)]);
+d3.csv('heatmap-gasses-2019.csv').then(res => {
+    data = res.map(d => [Number(d.lon), Number(d.lat)]);
     renderLayer();
 });
