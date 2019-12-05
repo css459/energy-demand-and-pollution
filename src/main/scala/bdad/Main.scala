a9e8cfe88d3ad0529a2877f31bf434cfb12b0006
Andrii <adobro.developer@gmail.com>
2019-12-05 03:57:10 -0500

    cleaned up cost score code, main, example csv

--- src/main/scala/bdad/Main.scala
+++ src/main/scala/bdad/Main.scala
@@ -1,7 +1,10 @@
 package bdad
 
+import bdad.etl.util
 import bdad.etl.Scenarios
+import bdad.etl.airdata.AirDataset
 import bdad.model.Heatmap.GassesHeatmap
+import bdad.model.CostRepresentation.CostRepresentation
 import bdad.model.TLCC.{TLCCIngress, TLCCModel}
 import org.apache.spark.rdd.RDD
 
@@ -75,8 +78,33 @@ object Main extends App {
     cors.collect.foreach(println)
   }
 
+  /**
+   * Triggers the generation of data from the HeatMap model
+   * for the heat map visualization. For testing and illustration
+   * purposes currently generates heat map data for 2019 only.
+   */
   def heatmap(): Unit = {
     GassesHeatmap.makeMap(2019, "heatmap-gasses-2019.csv")
+  }
+
+  /**
+   * Triggers the generation of pollution score data per area
+   * (lon, lat). For testing and illustration purposes generates
+   * the pollution scores for 2019 only.
+   */
+  def generatePollutionCostScores(): Unit = {
+    val air = new AirDataset(2019, "gasses/*")
+    val (petroleum, petroleum_labels) = Scenarios.petroleum2019test()
+
+    val airData = air.pivotedDF(dropNull = true, dropUnit = true)
+    val airDataColumns = air.validCriteria
+
+    // compute the pollution cost scores with lag = 10 and save to disk
+    val pollutionAreaScores = CostRepresentation.pollutionCostScores(airData, airDataColumns, petroleum, 10)
+    util.writeToDisk(pollutionAreaScores, "pollution-cost-scores-2019.csv")
+
+    // optional output to check that the DataFrame looks correct
+    print(pollutionAreaScores.show(false))
   }
 
   // Compute the correlation between the 2 signals
@@ -84,6 +112,9 @@ object Main extends App {
   correlationToxics()
   autocorrelation()
 
-  // Generate a heatmap for visualization of gas geo data
+  // Generate a heat map for visualization of gas geo data
   heatmap()
+
+  // Generate the pollution scores
+  generatePollutionCostScores()
 }
--- src/main/scala/bdad/model/CostRepresentation/CostRepresentation.scala
+++ src/main/scala/bdad/model/CostRepresentation/CostRepresentation.scala
@@ -1,50 +1,111 @@
+//
+// BDAD Final Project
+// CostRepresentation Model
+// Andrii Dobroshynskyi
+// CostRepresentation.scala
+//
+
 package bdad.model.CostRepresentation
 
-import bdad.etl.airdata.AirDataset
-import bdad.etl.util.{maxAbs, normalize, sma}
 import org.apache.spark.ml.linalg.{Vector, Vectors}
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.functions.col
+import bdad.etl.util.{normalize, sma}
 
 object CostRepresentation {
 
-  def computeCostScores(airDataset: AirDataset, petroleumData: DataFrame, year: Int): DataFrame = {
-    val piv = airDataset.pivotedDF(dropNull = true, dropUnit = true)
-    val smaDF = sma(piv, Array("lat", "lon"), airDataset.validCriteria, 10)
+  /**
+   * Calculations and transformations of DataFrames for computing the
+   * cost representation score of pollution per area (lon, lat) pairs
+   * Input is two DataFrames for Air data and Petroleum data, a list
+   * of columns of Air data, and the value of an optimum lag to compute
+   * a moving average for. This lag value can be computed experimentally
+   * by running the correlation() functions from Main.
+   *
+   * @param airData        Input DataFrame for Air data
+   * @param airDataColumns Labels for Air data
+   * @param petroleumData  Input DataFrame for Petroleum data
+   * @param lag            Optimum lag value to compute a moving average with
+   */
+  def pollutionCostScores(airData: DataFrame, airDataColumns: Array[String], petroleumData: DataFrame, lag: Int): DataFrame = {
 
-    val smaCols = smaDF.columns.filter(c => c.contains("_sma"))
-    val selectedCols = Array("lat", "lon", "dateGMT") ++ smaCols
+    // compute a simple moving average (SMA) of length lag
+    // `periods` determined experimentally and filter out
+    // columns that we do not need to build a
+    // normalized vector
 
-    val smaOnly = smaDF.select(selectedCols.head, selectedCols.tail: _*)
-    val norm =  normalize(smaOnly, smaCols, useMean = true, useStd = true)
+    val smaAirDF = sma(airData, Array("lat", "lon"), airDataColumns, lag)
+    val smaAirDFCols = smaAirDF.columns.filter(_.contains("_sma"))
 
-//    val dateVec = norm.select(Array("dateGMT", "scaled_features").head, Array("dateGMT", "scaled_features").tail: _*)
-//
-//    val selectColumns = Array("lat", "lon", "dateGMT", "scaled_features")
+    // normalize the SMA columns from the smaAirDF DataFrame
+    // and generate a `scaled_features` vector
+    // only keep the lon, lat, dateGMT and the columns need
+    // to compute the normalized vector by calling the
+    // `normalize function`
 
-//    val renamedCols = Array("lat_grouped", "lon_grouped", "dateGMT_grouped")
-    val groupedByLonLat = norm.groupBy("lat", "lon").agg(max("dateGMT").as("dateGMT"))
-    print(groupedByLonLat.show(false))
+    val normalizedAirDFCols = Array("lat", "lon", "dateGMT") ++ smaAirDFCols
+    val normalizedAirDF =  normalize(smaAirDF.select(normalizedAirDFCols.head, normalizedAirDFCols.tail: _*),
+      smaAirDFCols, useMean = true, useStd = true)
 
-    val vectorColumnsForJoin = Array("lat", "lon", "dateGMT", "scaled_features")
-    val withVector = norm.select(vectorColumnsForJoin.head, vectorColumnsForJoin.tail: _*)
-    print(withVector.show(false))
-
-    val joined = groupedByLonLat.join(withVector, Seq("lat", "lon", "dateGMT"), "inner")
+    // utility function to compute the L2 norm of a vector
 
     val norm_udf = udf((v: Vector) => Vectors.norm(v, 2))
-    val normalized = joined.withColumn("norms", norm_udf(col("scaled_features")))
 
-    val petroleumCols = Array("Date", "scaled_features")
-    val renamedCols = Array("dateGMT", "price_features")
-    val selectPetroleum = petroleumData.select(petroleumCols.head, petroleumCols.tail: _*).toDF(renamedCols.toSeq: _*)
+    // group the original DataFrame by lon, lat and aggregate
+    // on the most recent date in the DataFrame for that lon, lat
+    // pair
+    // this is equivalent to selecting unique lon, lat pairs
+    // with the latest dateGMT value across all rows of each
+    // lon, lat pair
 
-    val bothDF = normalized.join(selectPetroleum, "dateGMT").withColumn("price_norms", norm_udf(col("price_features")))
-    print(bothDF.show(false))
+    val groupedLonLatAirDF = normalizedAirDF
+      .groupBy("lat", "lon")
+      .agg(max("dateGMT").as("dateGMT"))
 
-    val resultCols = Array("lat", "lon", "score")
-    val result = bothDF.withColumn("score", col("norms") / col("price_norms"))
-    result
+    // narrow down the normalizedAirDF DataFrame to select
+    // only the values that we need in order to construct
+    // the final grouped Air DataFrame:
+    // `lat`, `lon`, `dateGMT` - fields in order to JOIN on
+    // `scaled_features` - field that we need in the final
+    // resulting grouped and joined DataFrame
+
+    val vectorAirDFCols = Array("lat", "lon", "dateGMT", "scaled_features")
+    val vectorAirDF = normalizedAirDF.select(vectorAirDFCols.head, vectorAirDFCols.tail: _*)
+
+    // join the DataFrame grouped by lat, lon and max(dateGMT) with
+    // the original DataFrame on 3 values:
+    //    `lat`, `lon`, `dateGMT`
+    // in order to get a result DataFrame that contains both the
+    // grouped lon, lat, but now also a corresponding `scaled_features`
+    // column and a column for its L2 norm
+    // for each unique lat, lon, dateGMT
+
+    val airDF = groupedLonLatAirDF
+      .join(vectorAirDF, Seq("lat", "lon", "dateGMT"), "inner")
+      .withColumn("norms", norm_udf(col("scaled_features")))
+
+    // transform the petroleum DataFrame by selecting only relevant
+    // columns and renaming them for clarity in the later JOIN
+
+    val petroleumDFCols = Array("Date", "scaled_features")
+    val petroleumDFColsRenamed = Array("dateGMT", "price_features")
+    val petroleumDF = petroleumData.select(petroleumDFCols.head, petroleumDFCols.tail: _*).toDF(petroleumDFColsRenamed.toSeq: _*)
+
+    // join the petroleum price data in petroleumDF with the air data
+    // in airDF grouped by lon, lat and max(dateGMT)
+
+    val jointDF = airDF
+      .join(petroleumDF, "dateGMT")
+      .withColumn("price_norms", norm_udf(col("price_features")))
+
+    // compute the score for each lat, lon pair and
+    // select only the lat, lon, score columns for the
+    // result DataFrame
+
+    val scoreDFCols = Array("lat", "lon", "score")
+    val scoreDF = jointDF
+      .withColumn("score", col("norms") / col("price_norms"))
+      .select(scoreDFCols.head, scoreDFCols.tail: _*)
+    scoreDF
   }
 }
